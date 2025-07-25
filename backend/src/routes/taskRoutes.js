b60242e305f83e844cace0a1ed87a10fa7b8cdbd
Neeraj Rajauriya <neerajrajauriya123@gmail.com>
2025-07-25 23:39:45 +0530

    Adding frontend and fixing some bug

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -4,6 +4,9 @@ import {
   getTasks,
   updateTask,
   deleteTask,
+  getUsers,
+  getTaskById,
+  updateTaskStatus
 } from '../contollers/taskContoller.js';
 import { verifyToken } from '../middlewares/authMiddleware.js';
 
@@ -11,7 +14,10 @@ const router = express.Router();
 
 router.post('/', verifyToken, createTask); // Create Task
 router.get('/', verifyToken, getTasks);    // Get Tasks
-router.put('/:id', verifyToken, updateTask); // Update Task
+router.get('/assignable-users', verifyToken, getUsers);
+router.get('/:id', verifyToken, getTaskById); 
+router.put('/:id', verifyToken, updateTask);          // For general task updates
+router.put('/:id/status', verifyToken, updateTaskStatus); // For status updates only
 router.delete('/:id', verifyToken, deleteTask); // Delete Task (CEO only)
 
 export default router;
